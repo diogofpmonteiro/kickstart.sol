e128ed900e659d6713e85295324691b58213d7f8
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2022-01-13 17:43:19 +0000

    update connection to metamask, display campaigns list

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
-import React from "react";
+// import React, { Component } from "react";
+import { useEffect, useState } from "react";
+import factory from "./../ethereum/factory";
 
-export default () => {
-  return <h1>Home page</h1>;
+const HomePage = () => {
+  const [campaigns, setCampaigns] = useState([]);
+
+  useEffect(() => {
+    const getCampaignList = async () => {
+      const campaignsList = await factory.methods.getDeployedCampaigns().call();
+      setCampaigns(campaignsList);
+    };
+    getCampaignList();
+  }, []);
+
+  //   console.log("campaigns list -", campaigns);
+  return (
+    <>
+      <div>Home</div>
+      <div>
+        <div>Campaigns list: </div>
+        {campaigns.length > 0 &&
+          campaigns.map((eachCampaign) => {
+            return eachCampaign;
+          })}
+      </div>
+    </>
+  );
 };
+
+export default HomePage;
