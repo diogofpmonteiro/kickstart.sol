ff981c1fe5c46624552fd57f80b71dcf8d1da4f3
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2022-01-14 00:43:38 +0000

    create new campaigns page

---
+++ pages/campaigns/new.js
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout";
+import "semantic-ui-css/semantic.min.css";
+
+const NewCampaign = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Create a Campaign</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default NewCampaign;
